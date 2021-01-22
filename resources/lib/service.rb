3401df166dee7440e4a88dd3dca0d1f21a18b78c
Miguel <manegron@redborder.com>
2021-01-22 14:04:29 +0000

    Adding a way to enable and disable a service

--- resources/lib/service.rb
+++ resources/lib/service.rb
@@ -5,6 +5,8 @@ class ServiceCmd < CmdParse::Command
     super('service')
     short_desc('Manage service actions and values')
     add_command(ServiceListCmd.new, default: true)
+    add_command(ServiceEnableCmd.new, default: true)
+    add_command(ServiceDisableCmd.new, default: true)
   end
 end
 
@@ -25,20 +27,75 @@ class ServiceListCmd < CmdParse::Command
 
     node = Chef::Node.load(Socket.gethostname.split(".").first)
     services = node.attributes.redborder.services
-    systemd_services = node.attributes.redborder.systemdservices
     services.each do |service,enabled|
       if $parser.data[:all_services] == false and enabled == false
         next
       else
-        systemd_services[service].each do |systemd_service|
-                ret = system("systemctl status #{systemd_service} &>/dev/null") ? "OK" : "Fail"
-                puts "Status of service #{systemd_service}: #{ret}"
-        end
+        ret = system("systemctl status #{service} &>/dev/null") ? "OK" : "Fail"
+        puts "Status of service #{service}: #{ret}"
       end
     end
 
   end
 end
 
+class ServiceEnableCmd < CmdParse::Command
+  def initialize
+    $parser.data[:all_services] = false
+    super('enable', takes_commands: false)
+    short_desc('Enable service from node')
+  end
+
+  def execute(service,*opt)
+
+    Chef::Config.from_file("/etc/chef/client.rb")
+    Chef::Config[:client_key] = "/etc/chef/client.pem"
+    Chef::Config[:http_retry_count] = 5
+
+    node = Chef::Node.load(Socket.gethostname.split(".").first)
+    services = node.attributes.redborder.services
+    if services.include? service
+      role = Chef::Role.load(Socket.gethostname.split(".").first)
+      if !role.override_attributes["redborder"]["services"][service] or (role.override_attributes["redborder"]["services"][service] and role.override_attributes["redborder"]["services"][service] == false)
+        role.override_attributes["redborder"]["services"][service] = true
+        role.save
+      else
+        puts "The service #{service} was already enabled."
+      end
+     else
+       puts "The service #{service} doesn't exists."
+     end
+  end
+end
+
+class ServiceDisableCmd < CmdParse::Command
+  def initialize
+    $parser.data[:all_services] = false
+    super('disable', takes_commands: false)
+    short_desc('Disable service from node')
+  end
+
+  def execute(service,*opt)
+
+    Chef::Config.from_file("/etc/chef/client.rb")
+    Chef::Config[:client_key] = "/etc/chef/client.pem"
+    Chef::Config[:http_retry_count] = 5
+
+    node = Chef::Node.load(Socket.gethostname.split(".").first)
+    services = node.attributes.redborder.services
+      if services.include? service
+      role = Chef::Role.load(Socket.gethostname.split(".").first)
+      if role.override_attributes["redborder"]["services"][service]
+        role.override_attributes["redborder"]["services"].delete(service)
+        role.save
+      else
+        puts "The service #{service} was already disabled."
+      end
+     else
+       puts "The service #{service} doesn't exists."
+     end
+
+  end
+end
 $parser.add_command(ServiceCmd.new)
 
