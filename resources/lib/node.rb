3490952354fba9409b79f4d87e361296a3dbdad0
David Vanhoucke <dvanhoucke@redborder.com>
2022-01-17 14:11:52 +0000

    add compact and alphabetically mode for nodes

--- resources/lib/node.rb
+++ resources/lib/node.rb
@@ -14,6 +14,8 @@ class NodeListCmd < CmdParse::Command
   def initialize
     super('list', takes_commands: false)
     short_desc('List nodes from cluster')
+    options.on('-a', '--alphabetically', 'Alphabetically ordered') { $parser.data[:alphabetically_ordered] = true }
+    options.on('-c', '--compact', 'Compact format') { $parser.data[:compact_format] = true }
     options.on('-x', '--extend', 'Extended information') { puts 'Extended information' }
   end
 
@@ -23,8 +25,19 @@ class NodeListCmd < CmdParse::Command
     response = Net::HTTP.get_response(uri)
     if response.code == "200"
       ret = JSON.parse(response.body)
-      ret.each do |member|
-        puts member["Name"]
+
+      # order alphabetically
+      if ($parser.data[:alphabetically_ordered])
+        ret = ret.sort{|a,b| (a["Name"]||"zzzzzzzz") <=> (b["Name"]||"zzzzzzzz")}
+      end
+
+      # show output in one line or multiple
+      if ($parser.data[:compact_format])
+        puts ret.map { |member| "#{member["Name"]}"}.join(" ").strip()
+      else 
+        ret.each do |member|
+          puts member["Name"]
+        end
       end
     else
       puts "Error: consul service not available"
