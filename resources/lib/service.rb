f745312c96620c4dd4b32c2547f657d7e4c9dd38
Miguel Negron <manegron@redborder.com>
2024-05-24 21:26:23 +0100

    Refactor colors not in use

--- resources/lib/service.rb
+++ resources/lib/service.rb
@@ -14,14 +14,9 @@ end
 
 class ServiceListCmd < CmdParse::Command
   RESET = "\033[0m"
-  BOLD = "\033[1m"
   RED = "\033[31m"
   GREEN = "\033[32m"
   YELLOW = "\033[33m"
-  BLUE = "\033[34m"
-  MAGENTA = "\033[35m"
-  CYAN = "\033[36m"
-  WHITE = "\033[37m"
 
   def initialize
     $parser.data[:all_services] = false
