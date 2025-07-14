17d73a05bd780d562ac8fb4a6b8f5dc8ecc4b002
ptorresred <ptorres@redborder.com>
2025-07-14 10:24:05 +0100

    Improvement/#22184 show manager node uptime (#58)
    * Improvement #22184: Show manager node uptime
    
    * Improvement #22184: Add space

--- resources/lib/service.rb
+++ resources/lib/service.rb
@@ -420,6 +420,13 @@ class ServiceListCmd < CmdParse::Command
       printf("-----------------------------------------------------------------\n")
     end
     printf("Running: #{running}  /  Stopped: #{stopped}  /  External: #{external}  /  Errors: #{errors}\n\n")
+    manager_node = utils.get_node(node_name)
+    if manager_node
+      printf("#{node_name} runtime: #{manager_node['uptime']}\n")
+      printf("#{node_name} start time: #{Time.now - manager_node['uptime_seconds']}\n\n")
+    else
+      printf("Error getting manager node\n\n")
+    end
   end
 
   def parse_memory_to_bytes(memory_str)
