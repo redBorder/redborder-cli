598e1cd17ca105b392237a5221c4de3c3a5086db
Miguel <manegron@redborder.com>
2026-02-27 20:18:38 +0000

    Add pre message on stopping service

--- resources/lib/service.rb
+++ resources/lib/service.rb
@@ -833,6 +833,7 @@ class ServiceStopCmd < CmdParse::Command
           found = false
           systemd_services.each_value do |v|
             if v.include?(service)
+              puts "Trying to stop service #{service} on #{n}"
               `systemctl stop #{service} &>/dev/null`
               ret = $?.success? ? 'stopped' : 'failed to stop'
               puts "#{service} #{ret} on #{n}"
